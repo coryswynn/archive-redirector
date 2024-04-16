1cd2fc6b48b326c2be13a6d0a5f5b6c32d6f8eeb
coryswynn <wynn.cory@gmail.com>
2024-04-16 15:46:01 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,11 +1,15 @@
 chrome.runtime.onInstalled.addListener(() => {
     const defaultSettings = {
-        "nytimes.com": true,
-        "barrons.com": true,
-        "fortune.com": true,
-        "wsj.com": true,
         "bloomberg.com": true,
-        "economist.com": true
+        "economist.com": true,
+        "forbes.com": true,
+        "fortune.com": true,
+        "foreignpolicy.com": true,
+        "latimes.com": true,
+        "nytimes.com": true,
+        "theathletic.com": true,
+        "washingtonpost.com": true,
+        "wsj.com": true
     };
     chrome.storage.local.set(defaultSettings);
 });
