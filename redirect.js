7a9f55b24aa3673368d36a245571143d37650697
coryswynn <wynn.cory@gmail.com>
2024-04-16 15:47:11 -0700

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,10 +1,39 @@
 const currentUrl = window.location.href;
 
+// Remove any existing hash from the URL to avoid interference
+const cleanUrl = currentUrl.split('#')[0];
+
+// Check Chrome storage to see if redirection is enabled for the domain
 chrome.storage.local.get(null, data => {
-    const sites = ["nytimes.com", "barrons.com", "fortune.com", "wsj.com", "bloomberg.com", "economist.com"];
-    const siteToRedirect = sites.find(site => currentUrl.includes(site) && data[site] !== false);
-    if (siteToRedirect) {
-        const archiveUrl = `http://archive.today/?run=1&url=${encodeURIComponent(currentUrl)}`;
-        window.location.replace(archiveUrl);
+    const sites = Object.keys(data).filter(site => data[site]);
+    const siteEnabled = sites.find(site => cleanUrl.includes(site) && data[site] !== false);
+
+    if (siteEnabled) {
+        // Define the new URL patterns for more specific site matching
+        const patterns = [
+            /^(https:\/\/www.bloomberg.com\/news\/[^?#]*)/,
+            /^(https:\/\/www.forbes.com\/sites\/[^?#]*)/,
+            /^(https:\/\/foreignpolicy.com\/\\d{4}\/\\d{2}\/\\d{2}\/.*?\/)/,
+            /^(https:\/\/www.nytimes.com\/.*\.html)/,
+            /^(https:\/\/www.economist.com\/.+)$/,
+            /^(https:\/\/www.latimes.com\/.+)$/,
+            /^(https:\/\/*.theathletic.com\/.+)$/,
+            /^(https:\/\/www.washingtonpost.com\/.*\/)/,
+            /^(https:\/\/www.wsj.com\/articles\/[^?#]*)/
+        ];
+
+        // Check each pattern to see if it matches the current URL
+        const siteToRedirect = patterns.find(pattern => cleanUrl.match(pattern));
+
+        if (siteToRedirect) {
+            const matchedUrl = cleanUrl.match(siteToRedirect)[1];
+            // Construct the URL for the archive page
+            const archiveUrl = `https://archive.ph/${matchedUrl}#archive-load`;
+            window.location.replace(archiveUrl);
+        } else {
+            console.log('URL does not match any specified patterns.');
+        }
+    } else {
+        console.log('Redirection for this site is not enabled or site is not specified.');
     }
 });
